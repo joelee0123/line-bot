817cee884750bb94f7e1d79b4b6ec48ecd0b5bee
JOELEE0123 <joelee650123@gmail.com>
2022-01-09 20:15:42 +0800

    update

--- app.py
+++ app.py
@@ -45,12 +45,13 @@ def handle_message(event):
 
     if msg in ['hi', 'Hi']:
         r = '嗨'
-    elif msg == '你吃飯了嗎'
+    elif msg == '你吃飯了嗎':
         r = '還沒'
-    elif msg == '你是誰'
+    elif msg == '你是誰':
         r = '我是機器人'
-    elif '訂位' in msg
+    elif '訂位' in msg:
         r = '您想訂位，是嗎?'
+
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=r))
